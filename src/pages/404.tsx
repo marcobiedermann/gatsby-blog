a1e4baad9d731a358170c914f2d7856b81fba379
Marco Biedermann <marco.biedermann@gmx.de>
2020-12-17 15:44:23 +0100

    fix: 404
    Fix 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,9 @@
 import { PageProps } from 'gatsby';
 import React, { FC } from 'react';
 import Layout from '../components/Layout';
-import SEO from '../components/SEO';
 
 const NotFoundPage: FC<PageProps> = () => (
   <Layout>
-    <SEO title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>

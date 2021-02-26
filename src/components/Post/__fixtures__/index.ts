e8a59b9c6e3b2cf4ba04bfe8162f7e8e21b79f24
Marco Biedermann <marco.biedermann@gmx.de>
2021-02-26 21:13:02 +0100

    refactor: Tags fixtures
    Refactor tags fixtures and reuse them in post component

--- src/components/Post/__fixtures__/index.ts
+++ src/components/Post/__fixtures__/index.ts
@@ -1,9 +1,10 @@
 import { PostProps } from '..';
+import tags from '../../Tags/__fixtures__';
 
 const post: PostProps = {
   date: '2000-01-01',
   html: '<p>Lorem Ipsum</p>',
-  tags: ['tag-1'],
+  tags: tags.tags,
   title: 'First Post',
   timeToRead: 1,
 };
---
+++ src/components/Tags/__fixtures__/index.ts
@@ -0,0 +1,7 @@
+import { TagsProps } from '..';
+
+const tags: TagsProps = {
+  tags: ['tag-1'],
+};
+
+export default tags;

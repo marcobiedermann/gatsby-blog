630a686b5305b61c165a1cda67962809d9b3857f
Marco Biedermann <marco.biedermann@gmx.de>
2021-02-27 09:33:36 +0100

    refactor: Typeface
    Replace typeface by fontsource

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,4 @@
+import '@fontsource/montserrat/latin-300-italic.css';
+import '@fontsource/montserrat/latin-300.css';
 import 'prism-theme-night-owl';
-import 'typeface-montserrat';
 import './src/styles/main.css';

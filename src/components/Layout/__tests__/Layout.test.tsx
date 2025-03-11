d52e60e9875c3f003ddefea458ac44810fdebe33
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com.>
2025-03-11 09:55:57 +0100

    fix: Fake Timer
    Mock current date in snapshot tests

--- src/components/Layout/__tests__/Layout.test.tsx
+++ src/components/Layout/__tests__/Layout.test.tsx
@@ -2,6 +2,15 @@ import { render } from '@testing-library/react';
 import React from 'react';
 import Layout from '..';
 
+beforeAll(() => {
+  jest.useFakeTimers();
+  jest.setSystemTime(new Date('2020-01-01'));
+});
+
+afterAll(() => {
+  jest.useRealTimers();
+});
+
 describe('layout component', () => {
   it('should render correctly', () => {
     const { container } = render(<Layout>Content</Layout>);
